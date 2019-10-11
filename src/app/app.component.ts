98411bc0ef88971337c6fae98f7f17b0e0123765
Steven Bruce <brucesc726@gmail.com>
2019-10-11 18:16:37 -0400

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'steven-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'steven';
+}
